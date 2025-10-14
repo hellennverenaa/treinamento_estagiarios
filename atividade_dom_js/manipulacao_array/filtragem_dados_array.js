f7c64c844aa3a33a39cce40119ef183d2ef116b8
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-10-14 15:53:19 -0300

    first files

---
+++ atividade_dom_js/manipulacao_array/filtragem_dados_array.js
@@ -0,0 +1,27 @@
+/*
+Filtragem de uma Lista de Dados (Array de Objetos)
+
+Objetivo: Praticar a manipulação de arrays, que é o coração de qualquer aplicação que exibe listas de dados (como a lista de reservas).
+
+Descrição:
+    O array de objetos chamado salas contém informações sobre diferentes salas disponíveis em um espaço. Cada objeto representa uma sala 
+    e deve ter as propriedades: nome (string), capacidade (number) e disponivel (boolean).
+
+    Ao carregar a página, use JavaScript para "desenhar" essa lista de salas na tela, mostrando o nome e a capacidade de cada uma.
+
+    Adicione dois elementos na página:
+
+        Um checkbox com o texto "Mostrar apenas salas disponíveis".
+
+        Um campo de texto (input) para "Filtrar por nome".
+
+    Faça com que, ao marcar/desmarcar o checkbox ou ao digitar no campo de texto, a lista de salas na tela seja atualizada dinamicamente para refletir os filtros.
+*/
+
+const salas = [
+    { nome: "Paixão", capacidade: 10, disponivel: true },
+    { nome: "Respeito", capacidade: 20, disponivel: false },
+    { nome: "Hoshin", capacidade: 15, disponivel: true },
+    { nome: "Auditorio Fab. 1", capacidade: 25, disponivel: true },
+    { nome: "Auditorio Fab. 2", capacidade: 30, disponivel: false }
+];
